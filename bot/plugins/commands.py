e0f230fd290df52d0db6e5cda5db1776e408a15e
N.A.S <88653689+konay1122@users.noreply.github.com>
2021-12-15 15:23:53 +0630

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -39,7 +39,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    '📫 SUPPORT', url="https://t.me/Ts_bots"
+                                    '📫 SUPPORT', url="https://t.me/allserieslinkchnel_rainbow"
                                 )
                         ]
                     ]
@@ -58,7 +58,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    '📫 SUPPORT', url="https://t.me/Ts_bots"
+                                    '📫 SUPPORT', url="https://t.me/allserieslinkchnel_rainbow"
                                 )
                         ]
                     ]
@@ -77,7 +77,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    '📫 SUPPORT', url="https://t.me/Ts_bots"
+                                    '📫 SUPPORT', url="https://t.me/allserieslinkchnel_rainbow"
                                 )
                         ]
                     ]
@@ -90,7 +90,7 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('📫 SUPPORT', url='https://t.me/Ts_bots'),
+        InlineKeyboardButton('📫 SUPPORT', url='https://t.me/allserieslinkchnel_rainbow'),
         InlineKeyboardButton('📕 ABOUT', callback_data="about")
     ],[
         InlineKeyboardButton('💡 HELP', callback_data="help"),
